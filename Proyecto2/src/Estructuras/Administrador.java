e1c8c511d24ad1650d30b112f7277bfe2f4fd274
georginaakel <georgina.akel@correo.unimet.edu.ve>
2024-02-28 12:55:22 -0400

    adelanto

--- Proyecto2/src/Estructuras/Administrador.java
+++ Proyecto2/src/Estructuras/Administrador.java
@@ -8,7 +8,7 @@ package Estructuras;
  *
  * @author Ana Blanco
  */
-public class Administrador {
+public class Administrador extends Thread {
     private Queue Cola1USM = new Queue(); 
     private Queue Cola2USM = new Queue(); 
     private Queue Cola3USM = new Queue(); 
@@ -17,5 +17,88 @@ public class Administrador {
     private Queue Cola3AVT = new Queue(); 
     private Queue ColaRefuerzoUSM = new Queue(); 
     private Queue ColaRefuerzoAVT = new Queue(); 
+    private Queue ganadoresUSM = new Queue();
+    private Queue ganadoresAVT = new Queue(); 
+
+    public Queue getCola1USM() {
+        return Cola1USM;
+    }
+
+    public void setCola1USM(Queue Cola1USM) {
+        this.Cola1USM = Cola1USM;
+    }
+
+    public Queue getCola2USM() {
+        return Cola2USM;
+    }
+
+    public void setCola2USM(Queue Cola2USM) {
+        this.Cola2USM = Cola2USM;
+    }
+
+    public Queue getCola3USM() {
+        return Cola3USM;
+    }
+
+    public void setCola3USM(Queue Cola3USM) {
+        this.Cola3USM = Cola3USM;
+    }
+
+    public Queue getCola1AVT() {
+        return Cola1AVT;
+    }
+
+    public void setCola1AVT(Queue Cola1AVT) {
+        this.Cola1AVT = Cola1AVT;
+    }
+
+    public Queue getCola2AVT() {
+        return Cola2AVT;
+    }
+
+    public void setCola2AVT(Queue Cola2AVT) {
+        this.Cola2AVT = Cola2AVT;
+    }
+
+    public Queue getCola3AVT() {
+        return Cola3AVT;
+    }
+
+    public void setCola3AVT(Queue Cola3AVT) {
+        this.Cola3AVT = Cola3AVT;
+    }
+
+    public Queue getColaRefuerzoUSM() {
+        return ColaRefuerzoUSM;
+    }
+
+    public void setColaRefuerzoUSM(Queue ColaRefuerzoUSM) {
+        this.ColaRefuerzoUSM = ColaRefuerzoUSM;
+    }
+
+    public Queue getColaRefuerzoAVT() {
+        return ColaRefuerzoAVT;
+    }
+
+    public void setColaRefuerzoAVT(Queue ColaRefuerzoAVT) {
+        this.ColaRefuerzoAVT = ColaRefuerzoAVT;
+    }
+
+    public Queue getGanadoresUSM() {
+        return ganadoresUSM;
+    }
+
+    public void setGanadoresUSM(Queue ganadoresUSM) {
+        this.ganadoresUSM = ganadoresUSM;
+    }
+
+    public Queue getGanadoresAVT() {
+        return ganadoresAVT;
+    }
+
+    public void setGanadoresAVT(Queue ganadoresAVT) {
+        this.ganadoresAVT = ganadoresAVT;
+    }
+    
     
 }
--- Proyecto2/src/Estructuras/InteligenciaArtificial.java
+++ Proyecto2/src/Estructuras/InteligenciaArtificial.java
@@ -8,6 +8,50 @@ package Estructuras;
  *
  * @author Ana Blanco
  */
-public class InteligenciaArtificial {
+public class InteligenciaArtificial extends Thread{
+    private int contadorParejas = 0;
+    private int dayDuration;
+    private int contadorTorneos = 0;
+//    
+//    private String [] nombresPersonajesUSM= {"Gary", "Benson", "Carl", "CJ", "Dr Henry", "Eileen", "Fantasmin", "Jennifer", 
+//        "Maellard", "Margarita", "Mordecai", "Musculoso", "Papaleta", "Rigby", "Roxy", "Skips", "Starla", "Techmo", "Thomas", "Waiter" };
+//    
+//    private String[] nombresPersonajesAVT= { "Aang", "Appa", "Avatar Roku", "Azula", "Hakoda", "Hama", "Iroh", "Jet", "Katara", 
+//        "Long Feng", "Mai", "Onji", "Ozai", "Pipsqueak", "Princesa Yue", "Principe Zuko", "Sokka","Toph Beifong", "Ty Lee"};
+//    
+//    private String [] urlPersonajesUSM = {"src//PersonajesUnShowMas/Gary.png", "src//PersonajesUnShowMas/benson.png", "src//PersonajesUnShowMas/carl.png", "src//PersonajesUnShowMas/cj.png", 
+//        "src//PersonajesUnShowMas/dr henry.png", "src//PersonajesUnShowMas/eileen.png", "src//PersonajesUnShowMas/fantasmin.png", "src//PersonajesUnShowMas/jennifer.png", "src//PersonajesUnShowMas/maellard.png", 
+//        "src//PersonajesUnShowMas/margarita.png", "src//PersonajesUnShowMas/mordecai.png", "src//PersonajesUnShowMas/musculoso.png", "src//PersonajesUnShowMas/papaleta.png", "src//PersonajesUnShowMas/rigby.png", 
+//        "src//PersonajesUnShowMas/roxy.png", "src//PersonajesUnShowMas/skips.png", "src//PersonajesUnShowMas/starla.png", "src//PersonajesUnShowMas/techmo.png", "src//PersonajesUnShowMas/thomas.png", "src//PersonajesUnShowMas/waiter.png" };
+//    
+//    private String[] urlPersonajesAVT = { "src//PersonajesAvatar/aang.png", "src//PersonajesUnShowMas/appa.png", "src//PersonajesUnShowMas/avatar roku.png", "src//PersonajesUnShowMas/azula.png", "src//PersonajesUnShowMas/hakoda.png", 
+//        "src//PersonajesUnShowMas/hama.png", "src//PersonajesUnShowMas/iroh.png", "src//PersonajesUnShowMas/jet.png", "src//PersonajesUnShowMas/katara.png", "src//PersonajesUnShowMas/long feng.png", "src//PersonajesUnShowMas/mai.png", 
+//        "src//PersonajesUnShowMas/onji.png", "src//PersonajesUnShowMas/ozai.png", "src//PersonajesUnShowMas/pipsqueak.png", "src//PersonajesUnShowMas/princesa yue.png", "src//PersonajesUnShowMas/principe zuko.png", 
+//        "src//PersonajesUnShowMas/sokka.png","src//PersonajesUnShowMas/toph beifong.png", "src//PersonajesUnShowMas/ty lee.png"};
+
+    public int getContadorParejas() {
+        return contadorParejas;
+    }
+
+    public void setContadorParejas(int contadorParejas) {
+        this.contadorParejas = contadorParejas;
+    }
+
+    public int getDayDuration() {
+        return dayDuration;
+    }
+
+    public void setDayDuration(int dayDuration) {
+        this.dayDuration = dayDuration;
+    }
+
+    public int getContadorTorneos() {
+        return contadorTorneos;
+    }
+
+    public void setContadorTorneos(int contadorTorneos) {
+        this.contadorTorneos = contadorTorneos;
+    }
+
     
 }
